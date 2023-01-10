466a29d987eebd3cddbf360662982bce0d1549fe
BaraMeyMey <kadethbara@myvuw.ac.nz>
2023-01-11 12:02:04 +1300

    Added certifcate for MySQL Database

--- contact.php
+++ contact.php
@@ -1,17 +1,36 @@
-<?php
-echo "Hello there";
-?>
 <!-- <?php
+echo "Hello there";
+?> -->
+
+<?php
   if (isset($_REQUEST['name']) && isset($_REQUEST['email']) && isset($_REQUEST['message'])) { 
 
-  // consulting db
-  $conn = new mysqli("localhost", "root", "", "MyFirstDB");
+
+//Consulting db for Azure
+  $server = "baramey.mysql.database.azure.com";
+  $username = "baramey";
+  $password = "Wellington123!";
+  $database = "WebDatabase";
+
+  $conn = mysqli_init();
+  mysqli_ssl_set($conn,NULL,NULL,"DigiCertGlobalRootCA.crt.pem",NULL,NULL);
+  mysqli_real_connect($conn, $server, $username, $password, $database);
+
+  // Create a MySQLi connection
+  // $conn = new mysqli($server, $username, $password, $database);
+
+
+// consulting db: for a localhost database
+  // $conn = new mysqli("localhost", "root", "", "MyFirstDB");
+
+
   if ($conn->connect_error) {
       echo "error";
       die("Connection failed: " . $conn->connect_error);
     }
 
-  // 1. I need to collect the data submitted by previous page (i.e. login.php)
+  // 1. I need to collect the data submitted by previous page (i.e. login.php): 
+  //The below will crash if the user inputs an apostrophe
   // $name = $_REQUEST ['name'];
   // $email = $_REQUEST['email'];
   // $message = $_REQUEST['message'];
@@ -30,4 +49,4 @@ echo "Hello there";
 
   echo "<script> window.location.href = \"index.php#contactMe\"; </script>";
   } 
-?> -->
+?>
